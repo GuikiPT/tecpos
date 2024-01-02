c3e681bb8961699a1d3ddab58675372062367743
GuikiPT <foxzillapt17@gmail.com>
2024-01-02 13:06:19 +0000

    Validation and formcontroller  builder  func

--- lib/pages/Login/login_popup.dart
+++ lib/pages/Login/login_popup.dart
@@ -1,6 +1,9 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
+import 'package:go_router/go_router.dart';
 import 'package:tecpos/components/numpad.dart';
 
 class LoginPopUp extends HookWidget {
@@ -18,6 +21,9 @@ class LoginPopUp extends HookWidget {
 
     final obscurePasswordProtection = useState(true);
 
+    final formKey = GlobalKey<FormBuilderState>();
+    final loginError = useState<String?>(null);
+
     useEffect(() {
       activeController = usernameController;
       activeFocusInput = usernameFocusNode;
@@ -25,72 +31,108 @@ class LoginPopUp extends HookWidget {
       return () {};
     }, []);
 
-    return Column(
-      children: [
-        TextFormField(
-          controller: usernameController,
-          onTap: () {
-            activeController = usernameController;
-            usernameFocusNode.requestFocus();
-            activeFocusInput = usernameFocusNode;
-          },
-          focusNode: usernameFocusNode,
-          keyboardType: TextInputType.none,
-          decoration: InputDecoration(
-            labelText: tr('screens.login.popUp.user'),
-            labelStyle: Theme.of(context).textTheme.bodySmall,
-          ),
-        ),
-        const SizedBox(
-          height: 10,
-        ),
-        TextFormField(
-          controller: passwordController,
-          onTap: () {
-            activeController = passwordController;
-            passwordFocusNode.requestFocus();
-            activeFocusInput = passwordFocusNode;
-          },
-          focusNode: passwordFocusNode,
-          obscureText: obscurePasswordProtection.value,
-          keyboardType: TextInputType.none,
-          decoration: InputDecoration(
-            labelText: tr('screens.login.popUp.password'),
-            labelStyle: Theme.of(context).textTheme.bodySmall,
-            suffixIcon: IconButton(
-              icon: Icon(obscurePasswordProtection.value
-                  ? Icons.visibility_off_outlined
-                  : Icons.visibility),
-              onPressed: () {
-                obscurePasswordProtection.value =
-                    !obscurePasswordProtection.value;
-              },
+    return FormBuilder(
+      key: formKey,
+      child: Column(
+        children: [
+          FormBuilderTextField(
+            name: 'username',
+            controller: usernameController,
+            onTap: () {
+              activeController = usernameController;
+              usernameFocusNode.requestFocus();
+              activeFocusInput = usernameFocusNode;
+            },
+            focusNode: usernameFocusNode,
+            keyboardType: TextInputType.none,
+            decoration: InputDecoration(
+              labelText: tr('screens.login.popUp.user'),
+              labelStyle: Theme.of(context).textTheme.bodySmall,
             ),
+            validator: FormBuilderValidators.compose([
+              FormBuilderValidators.numeric(),
+              FormBuilderValidators.required(),
+            ]),
           ),
-        ),
-        const SizedBox(
-          height: 10,
-        ),
-        POSNumpad(
-          onButtonPressed: (button) {
-            if (button == 'Clear') {
-              if (activeController.text.isNotEmpty) {
-                final newText = activeController.text
-                    .substring(0, activeController.text.length - 1);
-                activeController.text = newText;
-              }
-            } else if (button == 'ClearAll') {
-              activeController.text = '';
-            } else if (button == 'Done') {
-              // Handle done logic here
-            } else {
-              activeController.text += button;
-            }
+          const SizedBox(
+            height: 10,
+          ),
+          FormBuilderTextField(
+            name: 'password',
+            controller: passwordController,
+            onTap: () {
+              activeController = passwordController;
+              passwordFocusNode.requestFocus();
+              activeFocusInput = passwordFocusNode;
+            },
+            focusNode: passwordFocusNode,
+            obscureText: obscurePasswordProtection.value,
+            keyboardType: TextInputType.none,
+            decoration: InputDecoration(
+              labelText: tr('screens.login.popUp.password'),
+              labelStyle: Theme.of(context).textTheme.bodySmall,
+              suffixIcon: IconButton(
+                icon: Icon(obscurePasswordProtection.value
+                    ? Icons.visibility_off_outlined
+                    : Icons.visibility),
+                onPressed: () {
+                  obscurePasswordProtection.value =
+                      !obscurePasswordProtection.value;
+                },
+              ),
+            ),
+            validator: FormBuilderValidators.compose([
+              FormBuilderValidators.numeric(),
+              FormBuilderValidators.required(),
+            ]),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          if (loginError.value != null)
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  loginError.value!,
+                  style: const TextStyle(color: Colors.red),
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+              ],
+            ),
+          POSNumpad(
+            onButtonPressed: (button) {
+              if (button == 'Clear') {
+                if (activeController.text.isNotEmpty) {
+                  final newText = activeController.text
+                      .substring(0, activeController.text.length - 1);
+                  activeController.text = newText;
+                }
+              } else if (button == 'ClearAll') {
+                activeController.text = '';
+              } else if (button == 'Done') {
+                final formValid = formKey.currentState?.saveAndValidate();
 
-            activeFocusInput.requestFocus();
-          },
-        ),
-      ],
+                if (formValid == false) {
+                  return formKey.currentState?.saveAndValidate();
+                } else if (usernameController.text == '123' &&
+                    passwordController.text == '123') {
+                  loginError.value = null;
+                  context.go('/home');
+                } else {
+                  loginError.value =
+                      tr('screens.login.popUp.incorrectCredentials');
+                }
+              } else {
+                activeController.text += button;
+              }
+              activeFocusInput.requestFocus();
+            },
+          ),
+        ],
+      ),
     );
   }
 }
